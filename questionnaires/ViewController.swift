d54890eba9bcbdc20aa030c0a23b3b6bbf9d1046
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2019-08-03 08:05:38 +0630

    Add view controllers for UI

--- questionnaires/ViewController.swift
+++ questionnaires/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var data: String = ""
+
+    @IBOutlet weak var generalQuestionsButton: UIButton!
+    
+    @IBOutlet weak var programmingQuestionsButton: UIButton!
+    
+    @IBOutlet weak var CelebrityQuestionsButton: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        
+        switch sender.tag {
+        case 1:
+            data = "general"
+        case 2:
+            data = "programming"
+        case 3:
+            data = "celebrity"
+        default:
+            print("No action")
+        }
+        
+        performSegue(withIdentifier: "question1segue", sender: self)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let vc = segue.destination as? Question1
+        vc?.questionType = data
+        
+    }
+    
 }
 
